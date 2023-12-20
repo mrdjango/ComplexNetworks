dc32f2e5c45bc3c2965cc2bd20a02a17ec08fc03
Mostafa sh <152486442+mrdjango@users.noreply.github.com>
2023-12-20 22:47:34 +0000

    dev

--- Practice/practice-6/main.py
+++ Practice/practice-6/main.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,15 +24,19 @@ import numpy as np
 from torch_geometric.data import Data
 from torch_geometric.nn import GraphConv
 
+# %%
 # تبدیل گراف به بردارهای نمایشی
 def graph_to_vectors(graph):
-    adj_matrix = nx.to_numpy_matrix(graph)
+    adj_matrix = nx.to_numpy_array(graph)
     adj_matrix = torch.FloatTensor(adj_matrix)
-    x = F.relu(GraphConv(2, 16)(Data(x=adj_matrix)))
+    edge_index = torch.tensor(list(graph.edges)).t().contiguous()
+    x = F.relu(GraphConv(2, 16)(Data(x=adj_matrix, edge_index=edge_index)))
     x = F.relu(GraphConv(16, 32)(Data(x=x)))
     x = F.relu(GraphConv(32, 2)(Data(x=x)))
     return x
 
+# %%
+num_classes = 2
 # کلاس بند
 class Classifier(nn.Module):
     def __init__(self):
@@ -26,33 +48,52 @@ class Classifier(nn.Module):
         x = self.fc(x)
         return F.log_softmax(x, dim=1)
 
+# %%
 # تعریف گراف KarateClub
 karate_club = nx.karate_club_graph()
 
+
+# %%
 # تبدیل گراف به بردارهای نمایشی
+def graph_to_vectors(graph):
+    adj_matrix = nx.to_numpy_array(graph)
+    adj_matrix = torch.FloatTensor(adj_matrix)
+    edge_index = torch.tensor(list(graph.edges)).t().contiguous()
+    x = F.relu(GraphConv(34, 16)(x=adj_matrix, edge_index=edge_index))  # Adjust the input dimension to 34
+    x = F.relu(GraphConv(16, 32)(x=x, edge_index=edge_index))
+    x = F.relu(GraphConv(32, 2)(x=x, edge_index=edge_index))
+    return x
 vectors = graph_to_vectors(karate_club)
 
+# %%
 # تعریف کلاس‌ها
 labels = [karate_club.nodes[i]['club'] for i in karate_club.nodes]
 
+# %%
 # تعریف مدل
 num_classes = len(set(labels))
 model = Classifier()
 
+# %%
 # تعریف مسائل بهینه‌سازی و ترین مدل
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
+# %%
 # ترین مدل
 for epoch in range(100):
     optimizer.zero_grad()
     output = model(vectors)
-    loss = criterion(output, torch.LongTensor(labels))
+    labels_tensor = torch.LongTensor(labels)  # Convert labels to a tensor
+    loss = criterion(output, labels_tensor)
     loss.backward()
     optimizer.step()
     if epoch % 10 == 0:
         print(f'Epoch {epoch}, Loss: {loss.item()}')
 
+
+
+# %%
 # ارزیابی مدل
 with torch.no_grad():
     model.eval()
